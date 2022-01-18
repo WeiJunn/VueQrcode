033d6bf8b3b3b15b89c15010dc7c07a5a342740b
mmmmuqq <1564006443@qq.com>
2022-01-18 15:38:47 +0800

    Vueqrcode

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box">
+      <qr></qr>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Qr from "./components/qr.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: { Qr },
+};
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  padding: 0px;
+  margin: 0px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  border: hidden;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(to top right, #005aa7, #fffde4);
+}
+.box {
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/qr.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="box">
+    <vue-qr
+      :logoSrc="logoSrc"
+      :size="300"
+      :margin="0"
+      :auto-color="true"
+      :dot-scale="1"
+      :text="appSrc"
+    />
+    <div class="input">
+      <el-button type="primary" @click="generate()">生成</el-button>
+      <el-input
+        placeholder="请输入内容"
+        v-model.lazy="undetermined"
+        clearable
+        @keyup.enter.native="generate()"
+      >
+      </el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueQr from "vue-qr";
+
+export default {
+  data() {
+    return {
+      // logoSrc: require("@/assets/logo.png"),
+      logoSrc: "",
+      appSrc: "你输东西再识别啊，你是不是傻的",
+      undetermined: "", //待定生成 和输入框双向绑定 点击生成把值赋给appSrc
+    };
+  },
+  components: {
+    VueQr,
+  },
+  methods: {
+    generate() {
+      this.appSrc = this.undetermined;
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.box {
+  flex-direction: column;
+  width: 500px;
+  height: 500px;
+  background-color: rgb(255, 255, 255);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+}
+.input {
+  margin: 15px 15px 0 15px;
+  display: flex;
+  flex-direction: row-reverse;
+}
+</style>
